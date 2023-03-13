b7366bef38ea9b38bc44eb8d5a3ab68a3bb5f32f
CheThanh <chethanh3112@gmail.com>
2023-03-13 21:51:04 +0700

    productadd_demo

---
+++ admin/productAdd.php
@@ -0,0 +1,42 @@
+<?php include 'inc/header.php';?>
+<?php include 'inc/slidebar.php';?>
+<?php include '../classes/categoryC.php'?>
+<?php
+    $class = new categoryC();
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+        $cateName = $_POST['cateName'];
+        $cateDesc = $_POST['cateDesc'];
+        $insertCate=$class->insertCate($cateName, $cateDesc);
+    }
+?>
+<div >
+    <form action="addcatelist.php" method="post">
+        <div>
+            <?php
+            if(isset($insertCate)){
+                echo $insertCate;
+            }
+            ?>
+        </div>
+    <table>
+        <tr>
+            <td>
+                <input type="text" name="productNam" placeholder="Thêm tên danh mục...">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <textarea name="productDesc" id="" cols="30" rows="10" placeholder="Mô tả..."></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="submit" value="Thêm">
+            </td>
+        </tr>
+    </table>
+
+
+    </form>
+
+</div>

e332be491ed7ee9276d2fa3c9abd73192c5e3d01
CheThanh <chethanh3112@gmail.com>
2023-03-17 10:30:52 +0700

    style login_form

--- admin/login.php
+++ admin/login.php
@@ -19,11 +19,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../admin/assets/sytle.css">
     <title>Document</title>
 </head>
 <body class="text-center">
 <main class="form-signin w-100 m-auto">
-  <form action="login.php" method="post" id="login-form">
+  <form id="login-form">  <!--action="login.php" method="post"-->
     <h1 class="h3 mb-3 fw-normal">LOGIN</h1>
     <span>
         <?php
@@ -48,7 +49,7 @@
       </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
+    <p class="mt-5 mb-3 text-muted">© CheThanh</p>
   </form>
 </main>
  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 

76b63d706edb0936738d0722dce38f77109fb32f
CheThanh <chethanh3112@gmail.com>
2023-03-25 15:40:36 +0700

    MVC + EDIT product

--- admin/views/productList.php
+++ admin/views/productList.php
@@ -22,13 +22,14 @@
         <th>Tên sản phẩm</th>
         <th>Mô tả</th>
         <th>Hình ảnh</th>
+        <th>Trạng thái</th>
         <th>Loại</th>
         <th>Nổi bật</th>
         <th>Giá</th>
         <th>Thương hiệu</th>
     </tr>
     <?php
-        $showlistProduct = $product->showlistProduct();
+       
         if(isset($showlistProduct)){
             while($result=$showlistProduct->fetch_assoc()){
 
@@ -37,13 +38,23 @@
     ?>
         <tr>
             <td><?php echo $result['productName'];?></td>
-            <td><?php echo $result['productDesc'];?></td>
-            <td><?php echo $result['productImage'];?></td>
+            <td><?php echo $fm->textShorten($result['productDesc'],20);?></td>
+            <td><img src="uploads/product/<?php echo $result['productImage']?>" alt="product_image" width="80px"></td>
+            <td>
+                <?php if($result['productStatus']==0){
+                echo'<p>hết hàng</p>';
+                }else {echo'<p>còn hàng</p>';}
+                ?>
+
+            </td>
             <td><?php echo $result['category_id'];?></td>
-            <td><?php echo $result['productType'];?></td>
+            <td> <?php if($result['productType']==0){
+                echo'<p>Không nổi bật</p>';
+                }else {echo'<p>Nổi bật</p>';}
+                ?></td>
             <td><?php echo $result['productPrice'];?></td>
             <td><?php echo $result['brand_Id'];?></td>
-            
+            <td><a href="productEdit.php?productId=<?php echo $result['productId']?>">Edit</a>|<a onclick="return confirm('Bạn chắc chắn muốn xóa?')" href="?delproductId=<?php echo $result['productId']?>">Delete</a></td>
         </tr>
         <?php }
     } ?>
